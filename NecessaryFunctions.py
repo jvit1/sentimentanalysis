ec5cadbc79dadce74b47ac25fbfe84949016c250
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-05-21 18:57:37 -0400

    Update NecessaryFunctions.py

--- NecessaryFunctions.py
+++ NecessaryFunctions.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+
 '''
 The following file defines functions and values that are used throughout the scraper before outputting the cleaned dataset.
 '''
+
+
 # Spam Filter
 def spam_filter(df):
     df = df[~df[2].str.contains("RT")]
@@ -10,6 +13,7 @@ def spam_filter(df):
     df = df[~(df[11] <= 100)]  # Making sure total account likes is greater than 100
     df = df[(df[10] == False)]  # Making sure there is a real profile pic
     return df
+
 
 # Current Price
 source = requests.get('https://www.coindesk.com/price/bitcoin').text
