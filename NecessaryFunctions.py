3b87ae854da9bb06fb74f3cb30727865796d7273
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-05-25 10:39:32 -0400

    Update NecessaryFunctions.py

--- NecessaryFunctions.py
+++ NecessaryFunctions.py
@@ -2,25 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 from textblob import TextBlob
 import re
+import emoji
 
 '''
 The following file defines functions and values that are used throughout the scraper before outputting the cleaned dataset.
 '''
 
+
 # Cleans the majority of the doo doo out of the tweet text
-def cleanTxt(text):
-    text = re.sub(r'@[A-Za-z0-9]+', '', text)  # no @s
+def CleanText(text):
+    text = re.sub(r'@[A-Za-z0-9_]+', '', text)  # no @s
     text = re.sub(r'#', '', text)  # no hashtag symbols
-    text = re.sub(r'https?:\/\/\S+', '', text)  # remove hyperlink
+    text = re.sub(r'https?:\/\/\S+', '', text)  # Remove image links
+    text = re.sub(r'\$[A-Z]+', '', text)  # Remove currency labels
+    text = re.sub(r'\n', '', text)  # remove hyperlink
     return text
 
+
+# Removes emojis
+def remove_emoji(text):
+    return emoji.get_emoji_regexp().sub(u'', text)
+
+
 # Subjectivity
-def getSubjectivity(text):
+def Subjectivity(text):
     return TextBlob(text).sentiment.subjectivity
 
+
 # Polarity
-def getPolarity(text):
+def Polarity(text):
     return TextBlob(text).sentiment.polarity
+
 
 # Current Price
 source = requests.get('https://www.coindesk.com/price/bitcoin').text
