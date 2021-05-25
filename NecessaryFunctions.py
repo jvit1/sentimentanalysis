d940686033e0d2a53e273c3a03fcf068351432c6
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-05-24 21:35:33 -0400

    Update NecessaryFunctions.py

--- NecessaryFunctions.py
+++ NecessaryFunctions.py
@@ -1,29 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from textblob import TextBlob
 import re
-from nltk.corpus import stopwords
 
 '''
 The following file defines functions and values that are used throughout the scraper before outputting the cleaned dataset.
 '''
 
-
+# Cleans the majority of the doo doo out of the tweet text
 def cleanTxt(text):
     text = re.sub(r'@[A-Za-z0-9]+', '', text)  # no @s
     text = re.sub(r'#', '', text)  # no hashtag symbols
     text = re.sub(r'https?:\/\/\S+', '', text)  # remove hyperlink
     return text
 
-# Spam Filter - Find way to apply this
-def spam_filter(df):
-    df = df[~df.Text.astype(str).str.contains("RT")]
-    df = df[~(df.UserFollowingCount <= 100)]  # Making sure total followers is greater than 100
-    df = df[~(df.TotalAccountLikes <= 100)]  # Making sure total account likes is greater than 100
-    df = df[(df.DefaultProfileImage == False)]
-    return df
+# Subjectivity
+def getSubjectivity(text):
+    return TextBlob(text).sentiment.subjectivity
 
+# Polarity
+def getPolarity(text):
+    return TextBlob(text).sentiment.polarity
 
 # Current Price
 source = requests.get('https://www.coindesk.com/price/bitcoin').text
