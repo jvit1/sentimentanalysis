a55a02de1faf1fd0059b748079503b6980c91a71
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-05-24 20:42:37 -0400

    Update NecessaryFunctions.py

--- NecessaryFunctions.py
+++ NecessaryFunctions.py
@@ -1,12 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from textblob import TextBlob
+import re
+from nltk.corpus import stopwords
 
 '''
 The following file defines functions and values that are used throughout the scraper before outputting the cleaned dataset.
 '''
 
 
-# Spam Filter
+def cleanTxt(text):
+    text = re.sub(r'@[A-Za-z0-9]+', '', text)  # no @s
+    text = re.sub(r'#', '', text)  # no hashtag symbols
+    text = re.sub(r'https?:\/\/\S+', '', text)  # remove hyperlink
+    return text
+
+# Spam Filter - Find way to apply this
 def spam_filter(df):
     df = df[~df.Text.astype(str).str.contains("RT")]
     df = df[~(df.UserFollowingCount <= 100)]  # Making sure total followers is greater than 100
