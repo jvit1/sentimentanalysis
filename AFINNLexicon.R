3cc2fd34d982b79bd8968a112f0604a4c4cf8fc5
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-06-12 19:13:38 -0400

    Create AFINNLexicon.R

---
+++ AFINNLexicon.R
@@ -0,0 +1,48 @@
+#Packages
+library(readr)
+library(tidyverse)
+library(tidytext)
+library(gridExtra)
+library(grid)
+library(igraph)
+library(ggraph)
+library(widyr)
+library(SnowballC)
+library(readr)
+
+link = 'https://raw.githubusercontent.com/jvit1/sentimentanalysis/main/Scraped_Tweets.csv'
+data <- read_csv(url(link))
+#data <- read.csv("Scraped_Tweets.csv")
+
+#Tokenizes and removes stopwords. 
+review.words <- data %>%
+  select(Text, `BTC Price`, Date) %>%
+  unnest_tokens(word, Text) %>%
+  mutate(word = str_extract(word, "[0-9a-z']+")) %>%
+  drop_na()
+
+review.words$word <- sapply(review.words$word, removeNumbers)
+
+review.words <- review.words %>%
+  anti_join(stop_words)
+
+
+#Summarizes by count.
+review.words %>% group_by(word) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+
+# For now, we'll use the afinn lexicon becuase we want a quantitatve value. 
+# We average out the scores by date. This will be what we can graph from.
+
+afinn.sent <- get_sentiments("afinn")
+
+sentiments <- review.words %>%
+  inner_join(afinn.sent)
+
+sentiments$Date <- as.Date(sentiments$Date)
+
+final.table <- sentiments %>% select(`BTC Price`, Date, value) %>%
+  group_by(Date, `BTC Price`) %>% summarize(score = mean(value))
+
+
