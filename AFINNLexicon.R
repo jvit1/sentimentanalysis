9f657c3ac51653ece4b65101a49d55d953710f14
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-06-13 12:21:16 -0400

    Update AFINNLexicon.R

--- AFINNLexicon.R
+++ AFINNLexicon.R
@@ -1,4 +1,4 @@
-#Packages
+# Packages
 library(readr)
 library(tidyverse)
 library(tidytext)
@@ -12,9 +12,9 @@ library(readr)
 
 link = 'https://raw.githubusercontent.com/jvit1/sentimentanalysis/main/Scraped_Tweets.csv'
 data <- read_csv(url(link))
-#data <- read.csv("Scraped_Tweets.csv")
 
-#Tokenizes and removes stopwords. 
+
+# Tokenizes and removes stopwords. 
 review.words <- data %>%
   select(Text, `BTC Price`, Date) %>%
   unnest_tokens(word, Text) %>%
@@ -27,12 +27,12 @@ review.words <- review.words %>%
   anti_join(stop_words)
 
 
-#Summarizes by count.
+# Summarizes by count.
 review.words %>% group_by(word) %>%
   summarize(count = n()) %>%
   arrange(desc(count))
 
-# For now, we'll use the afinn lexicon becuase we want a quantitatve value. 
+# For now, we'll use the AFINN lexicon because we want a quantitative value. 
 # We average out the scores by date. This will be what we can graph from.
 
 afinn.sent <- get_sentiments("afinn")
@@ -46,3 +46,51 @@ final.table <- sentiments %>% select(`BTC Price`, Date, value) %>%
   group_by(Date, `BTC Price`) %>% summarize(score = mean(value))
 
 
+write.csv(final.table,"C:/Users/student/Documents/UVA/Portfolio Projects/Sentiment Analysis/sentimentanalysis/Data/AFINNLexicon.csv", row.names = FALSE)
+
+# Let's also get a csv of most common words (for graphic purposes).
+word.totals <- review.words %>% group_by(word) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+
+write.csv(word.totals,"C:/Users/student/Documents/UVA/Portfolio Projects/Sentiment Analysis/sentimentanalysis/Data/wordtotals.csv", row.names = FALSE)
+
+# Bigrams: We'll use the same approach as before.
+bigrams <- data %>%
+  unnest_tokens(bigram, Text, token = "ngrams", n = 2) %>%
+  mutate(bigram = str_extract(bigram, "[0-9a-z'\\s]+")) %>%
+  drop_na()
+
+bigrams <- bigrams %>%
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>%
+  drop_na() %>%
+  unite(bigram, word1, word2, sep = " ") 
+
+## We are going to define some additional stop words to remove. 
+##Because we are interested in seeing what else people are talking about, we will remove 'bitcoin'.
+
+###############################################################################
+new_stop_words <- tibble(word.stem = c("bitcoin", "btc", ""))
+
+test <- bigrams %>%
+  separate(bigram, c("word1", "word2"), sep= ' ') %>%
+  filter(!word1 %in% new_stop_words) %>%
+  filter(!word2 %in% new_stop_words) %>%
+  unite(bigram, word1, word2, sep = " ")
+###################### Bigram word removal is not working #####################
+################### FIX THIS ASAP #######################
+
+bigram.count <- bigrams %>%
+  group_by(bigram) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+
+
+bigram.count %>%
+  top_n(15, count) %>%
+  mutate(bigram = reorder(bigram, count)) %>%
+  ggplot(aes(x = bigram, y = count)) + 
+  geom_col() + 
+  coord_flip()
