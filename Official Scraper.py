05d255bbe2bd260a0d37b894f1d7124420057218
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-06-21 15:51:49 -0400

    Update Official Scraper.py

--- Official Scraper.py
+++ Official Scraper.py
@@ -63,6 +63,84 @@ tweets_df['Polarity'] = tweets_df['Text'].apply(Polarity)
 
 tweets_df = tweets_df[(tweets_df.Polarity != 0)]  # Making sure polarity is not 0
 
+print("Cleaning Locations")
+# Fixing Country Location
+# Australia
+australia_cities = "Queensland|Sydney|Australia|Melbourne"
+tweets_df.loc[tweets_df['User Location'].str.contains(australia_cities, case=False, na=False), 'User Location'] = 'Australia'
+
+# Bangledesh
+bangladesh_cities = "Satkhira|Bangladesh"
+tweets_df.loc[tweets_df['User Location'].str.contains(bangladesh_cities, case=False, na=False), 'User Location'] = 'Bangladesh'
+
+# Brazil
+brazil_cities = "Goiânia|Janeiro|Brasil|Brazil|Sao Paulo"
+tweets_df.loc[tweets_df['User Location'].str.contains(brazil_cities, case=False, na=False), 'User Location'] = 'Brazil'
+
+# Canada
+canada_cities = "Toronto|Ontario|Canada|Vancouver|Ottawa|Montréal|Québec|Alberta"
+tweets_df.loc[tweets_df['User Location'].str.contains(canada_cities, case=False, na=False), 'User Location'] = 'Canada'
+
+# Cyprus
+cyprus_cities = "Ayia"
+tweets_df.loc[tweets_df['User Location'].str.contains(cyprus_cities, case=False, na=False), 'User Location'] = 'Cyprus'
+
+
+# France
+france_cities = "France|Paris"
+tweets_df.loc[tweets_df['User Location'].str.contains(france_cities, case=False, na=False), 'User Location'] = 'France'
+
+
+# Indonesia
+indonesia_cities = "Jakarta|Indonesia|Bekasi|Java|Jawa"
+tweets_df.loc[tweets_df['User Location'].str.contains(indonesia_cities, case=False, na=False), 'User Location'] = 'Indonesia'
+
+# India
+india_cities = "Mumbai|India|bangalore|Nairobi|New Delhi"
+tweets_df.loc[tweets_df['User Location'].str.contains(india_cities, case=False, na=False), 'User Location'] = 'India'
+
+# Kenya
+kenya_cities = "Kisumu"
+tweets_df.loc[tweets_df['User Location'].str.contains(kenya_cities, case=False, na=False), 'User Location'] = 'Kenya'
+
+# Malaysia
+Malaysia_cities = "Malaysia|Kuala"
+tweets_df.loc[tweets_df['User Location'].str.contains(Malaysia_cities, case=False, na=False), 'User Location'] = 'Malaysia'
+
+# Nigeria
+nigeria_cities = "Nigeria"
+tweets_df.loc[tweets_df['User Location'].str.contains(nigeria_cities, case=False, na=False), 'User Location'] = 'Nigeria'
+
+# Philippines
+pilippines_cities = "Caloocan|Philippines"
+tweets_df.loc[tweets_df['User Location'].str.contains(pilippines_cities, case=False, na=False), 'User Location'] = 'Philippines'
+
+#South Africa
+south_africancities = "Midrand|South Africa"
+tweets_df.loc[tweets_df['User Location'].str.contains(south_africancities, case=False, na=False), 'User Location'] = 'South Africa'
+
+# Thailand
+thailand_cities = "Bangkok"
+tweets_df.loc[tweets_df['User Location'].str.contains(thailand_cities, case=False, na=False), 'User Location'] = 'Thailand'
+
+# United States
+us_cities = "Angeles|York|Greenville|Michigan|Tampa|Fayetteville|Antonio|Manhattan|USA|Boston|Detroit|United States|" \
+            "Dallas|Oklahoma|Jersey|Chicago|Albuquerque|Orlando|NYC|San Francisco|Leesburg|Ohio|Miami|Stamford|SEATTLE|" \
+            "TX|Gainesville|Denver|Bay Area|Philly|Florida|KY|St Joseph|Vegas|Providence|Myrtle|Pennsylvania|Washington|Richmond|" \
+            "Texas|Savannah|Salt Lake|New Orleans|Cary|Long Island|Concrete Jungle|united state|colorado|" \
+            "grand rapids|Louisville|Charlotte|Atlanta|Jacksonville|Inglewood|Scottsdale|Palm Beach|Columbus|Kansas City|" \
+            "California|Palo Alto|Columbus|Roanoke|Durham|Rochester|Beverly Hills|Utah|America|Madison|Philadelp|Hollywood|" \
+            "Boca Raton|Portland|Nashville|Tacoma|Wellington|Bethesda|Roseville|Bristol|Lafayette|San Diego|Night City|" \
+            "Lauderdale|Wildwood|NJ|San Jose|TN|Hawaii|Brooklyn|Bethlehem|Orange County|Birmingham|Virginia|FL|Victoria|" \
+            "Silicon Valley|Brookline|Allentown|Pasadena|Queens|Nevada|Santa Clara|Lancaster|Bellevue|Marina del|Rockville|" \
+            "Darien|Mesa|Oshkosh|Laguna|Baltimore|Rochelle|Ridgefield|Castle Rock|Lake Forest|St Louis|Tyson|Santa Rosa|" \
+            "Alexandria|Sacramento"
+tweets_df.loc[tweets_df['User Location'].str.contains(us_cities, case=False, na=False), 'User Location'] = 'United States of America'
+
+#United Kingdom
+uk_cities = "London|Liverpool|England|Scotland|UK|Ireland|United Kingdom|Wales"
+tweets_df.loc[tweets_df['User Location'].str.contains(uk_cities, case=False, na=False), 'User Location'] = 'United Kingdom'
+
 
 # mode='a' to append once ready for production
 tweets_df.to_csv("Scraped_Tweets.csv", header=False, mode='a')
