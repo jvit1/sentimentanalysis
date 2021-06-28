8b433033de0df0befc7855f72560f377a1a2ad85
jvit1 <81688613+jvit1@users.noreply.github.com>
2021-06-28 08:30:50 -0400

    Update TweetJoiner.R

--- TweetJoiner.R
+++ TweetJoiner.R
@@ -1,4 +1,5 @@
 library(ecospace)
+
 setwd('C:/Users/student/Documents/UVA/Portfolio Projects/Sentiment Analysis/sentimentanalysis/TimeStamped/')
 
 # Formatting today's tweets
@@ -12,4 +13,3 @@ write.csv(data.today, paste(path, date.today, ".csv", sep = ""), row.names = FAL
 all.tweets <- lapply(Sys.glob("Tweets*.csv"), read.csv)
 all.tweets <- rbind_listdf(lists = all.tweets, seq.by = 100)
 write.csv(all.tweets,"C:/Users/student/Documents/UVA/Portfolio Projects/Sentiment Analysis/sentimentanalysis/Scraped_Tweets.csv", row.names = FALSE)
-
